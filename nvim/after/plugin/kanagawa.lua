ea4b075ddc1e815741f976a64d3f72a7e2c282fd
Joshua Santana <inf2933@hs-worms.de>
2024-09-03 20:38:29 +0200

    update nvim conf

---
+++ nvim/after/plugin/kanagawa.lua
@@ -0,0 +1,13 @@
+require("kanagawa").setup({
+	keywordStyle = { italic = false },
+	colors = {
+		theme = {
+			all = {
+				ui = {
+					bg_gutter = "none",
+					bg_m3 = "#2A2A37",
+				},
+			},
+		},
+	},
+})
--- nvim/init.lua
+++ nvim/init.lua
@@ -8,24 +8,24 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.updatetime = 50
 vim.opt.scrolloff = 8
-vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
-vim.g.sonokai_style = 'andromeda'
+vim.g.sonokai_style = "andromeda"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 -- vim.lsp.set_log_level("debug")
-vim.api.nvim_set_var('mapleader', ' ');
+vim.api.nvim_set_var("mapleader", " ")
 
 require("lazy").setup("plugins")
-vim.cmd [[colorscheme kanagawa]]
+vim.cmd([[colorscheme kanagawa]])
