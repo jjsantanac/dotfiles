2eb442d7397f76f5662f7d4709fbf77dfe63bc8b
Joshua Santana <inf2933@hs-worms.de>
2024-09-26 22:26:10 +0200

    update nvim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,64 +1,78 @@
-local lsp = require("lsp-zero").preset({
-	name = "recommended",
-	set_lsp_keymaps = false,
-	suggest_lsp_servers = false,
-	sign_icons = false,
+vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	end,
 })
 
-lsp.configure("yamlls", {
-	settings = {
-		yaml = {
-			keyOrdering = false,
-			schemas = {
-				["/Users/joshuasantana/Documents/projects/deployment-konfiguration/dp.schema.json"] = "*.dp.yaml",
-			},
-		},
-	},
-})
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({ buffer = bufnr })
-end)
+local default_setup = function(server)
+	require("lspconfig")[server].setup({
+		capabilities = lsp_capabilities,
+	})
+end
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"lua_ls",
-		"tsserver",
-		"eslint",
-		"rust_analyzer",
-		"angularls@17.1.1",
-		"pylsp",
-		"cssls",
-		"jsonls",
-		"html",
-		"yamlls",
-	},
-
+	ensure_installed = {},
 	handlers = {
-		lsp.default_setup,
-		-- lua_ls = function()
-		--   local lua_opts = lsp.nvim_lua_ls()
-		--   require('lspconfig').lua_ls.setup(lua_opts)
-		-- end,
-		-- tsserver = function()
-		--   require('lspconfig').tsserver.setup({})
-		-- end,
-		-- eslint = function()
-		--   require('lspconfig').eslint.setup({})
-		-- end,
+		default_setup,
+		lua_ls = function()
+			require("lspconfig").lua_ls.setup({
+				capabilities = lsp_capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = {
+								vim.env.VIMRUNTIME,
+							},
+						},
+					},
+				},
+			})
+		end,
 	},
 })
 
 local cmp = require("cmp")
 
 cmp.setup({
-	mapping = {
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+	mapping = cmp.mapping.preset.insert({
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
 	},
 })
+
 vim.diagnostic.config({
 	update_in_insert = true,
 	float = {
